57a328a09b0904dc0d0f6ba89bf531b8b5acb811
Noah Zakaib <ndz@sfu.ca>
2026-02-07 23:16:39 -0800

    Legacy Open GL Test

--- src/main.cpp
+++ src/main.cpp
@@ -26,11 +26,17 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
         /* Poll for and process events */
         glfwPollEvents();
+
+        
     }
 
     glfwTerminate();
